164942ea355e4cb045b3b5cf1bbbb9d09d5d3c55
Mark Gallardo <mrk_gllrd@yahoo.com>
2020-02-01 20:05:49 -0600

    added package.json and started server.js

---
+++ server.js
@@ -0,0 +1,35 @@
+// Require Dependencies
+var express = require("express");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require('cheerio');
+var exphs = require("express-handlebars");
+
+// Set the PORT
+var PORT = process.env.PORT || 3000;
+
+// Initialize Express
+var app = express();
+
+// Define Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Initialize Handlebars
+// Boilerplate code from npmjs.com
+app.engine('handlebars', exphs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+// require routes
+require()
+
+app.listen(PORT, function () {
+    console.log("App listening on http://localhost:" + PORT);
+});
